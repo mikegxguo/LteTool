0908a74b8c1cc40fa9b8e15302dc43c34de8c8fe
mike.gou <gxgou.gou@mic.com.tw>
2019-04-04 11:08:37 +0800

    [TOBY-L2][Trace]Support to capture trace log from the UART port. [Description] None;

--- src/com/mitac/lte/trace.java
+++ src/com/mitac/lte/trace.java
@@ -102,9 +102,10 @@ public class trace extends Activity {
     public void onLogEnabled(View view) {
         if(curstate==1 || curstate ==5) {
             //String[] oemhookstring = { "AT+TRACE=1,921600" + '\r' };//LISA U200
-            String[] oemhookstring = { "AT+USYSTRACE=0,\"bb_sw=1\",\"bb_sw=sdl:th,tr,st,pr,mo,lt,db,li,gt,ae|fts:sdl(gprs,umts)|lte_stk:0x01,0xFFFFFFFF,0xFFFFFFFF,0xFFFFFFFF,0xFFFFFFFF|lte_stk:0x02,0x801FFFFF|ims:1\",\"oct=4\",921600" + '\r' }; //LARA R211
+            //String[] oemhookstring = { "AT+USYSTRACE=0,\"bb_sw=1\",\"bb_sw=sdl:th,tr,st,pr,mo,lt,db,li,gt,ae|fts:sdl(gprs,umts)|lte_stk:0x01,0xFFFFFFFF,0xFFFFFFFF,0xFFFFFFFF,0xFFFFFFFF|lte_stk:0x02,0x801FFFFF|ims:1\",\"oct=4\",921600" + '\r' }; //LARA R211
             //AT+TRACE=1,460800,,,,,"CDC-ACM"
             //String[] oemhookstring = { "AT+TRACE=1,460800,,,,,\"CDC-ACM\"" + '\r' };//TOBY-L2
+            String[] oemhookstring = { "AT+TRACE=1,115200,,,,,\"UART\"" + '\r' };//TOBY-L2
             // Create message
             Message msg = mHandler
                 .obtainMessage(EVENT_RIL_OEM_HOOK_CMDSTR_COMPLETE);
@@ -120,6 +121,12 @@ public class trace extends Activity {
         if(curstate==2 || curstate==4) {
             SystemProperties.set("gsm.ulog.service.init", "start");
             curstate = 3;
+            String[] oemhookstring = { "AT+CFUN=1,1" + '\r' };//TOBY-L2
+            // Create message
+            Message msg = mHandler
+                .obtainMessage(EVENT_RIL_OEM_HOOK_CMDSTR_COMPLETE);
+            // Send request
+            mPhone.invokeOemRilRequestStrings(oemhookstring, msg);
             //Toast.makeText(trace.this, "Start ulog service", Toast.LENGTH_LONG).show();
             CmdRespText = (EditText) findViewById(R.id.edit_response);
             CmdRespText.setText("---Start ulog service---");
@@ -156,8 +163,8 @@ public class trace extends Activity {
 
     public void onLogDisabled(View view) {
         if(curstate == 4) {
-            //String[] oemhookstring = { "AT+TRACE=0" + '\r' };//TOBY-L2
-            String[] oemhookstring = { "AT+USYSTRACE=0" + '\r' }; //LARA R211
+            String[] oemhookstring = { "AT+TRACE=0" + '\r' };//TOBY-L2
+            //String[] oemhookstring = { "AT+USYSTRACE=0" + '\r' }; //LARA R211
             // Create message
             Message msg = mHandler
                 .obtainMessage(EVENT_RIL_OEM_HOOK_CMDSTR_COMPLETE);
@@ -238,8 +245,10 @@ public class trace extends Activity {
                     break;
                 case EVENT_RIL_OEM_HOOK_CMDSTR_COMPLETE:
                     log("EVENT_RIL_OEM_HOOK_CMDSTR_COMPLETE");
-                    ar = (AsyncResult) msg.obj;
-                    logRilOemHookResponseString(ar);
+                    if(curstate != 3) {//Exclude this case
+                        ar = (AsyncResult) msg.obj;
+                        logRilOemHookResponseString(ar);
+                    }
                     break;
                 case EVENT_UNSOL_RIL_OEM_HOOK_RAW:
                     break;
